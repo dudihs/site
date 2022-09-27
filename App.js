4c105a5166ce09a90fbd099e0293c2ace1e64f9d
dudihs <112397828+dudihs@users.noreply.github.com>
2022-09-27 10:52:58 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-
-import { View, Text } from 'react-native';
-import Detalhes from './sources/telas/carrinho/Detalhes';
-import Topo from './sources/telas/carrinho/Topo';
-import mocks from "./sources/mocks/carrinho";
-
+import {View,Text, FlatList } from "react-native";
+import Detalhes from "./sources/telas/Carrinho/Detalhes";
+import Topo from "./sources/telas/Carrinho/Topo";
+import Mocks from ".sources/mocks/carrinho";
+import Item from "./sources/telas/Carrinho/Item";
 export default function App() {
   return (
     <View>
       <Text>Principal</Text>
-      <Topo {...mocks.topo} />
-      <Detalhes />
+      <Topo {...Mocks.Topo}/>
+      <Detalhes {...Mocks.Detalhes}/>
+      
+      <FlatList
+       data={Mocks.itens.lista}
+       renderItem={Item}
+       keyExtractor={({nome})=>nome}
+      />
     </View>
-  )
+  );
 }
